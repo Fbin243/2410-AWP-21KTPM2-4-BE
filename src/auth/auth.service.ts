c8bba8908cf60683ec6eb8a750d5a0af42b6dea3
Nguyễn Thanh Bình <ntbinh243.dev@gmail.com>
2024-11-12 00:51:19 +0000

    refactor: adjust logic logic to return basic info along with token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -26,13 +26,15 @@ export class AuthService {
   async signIn(
     email: string,
     password: string,
-  ): Promise<{ access_token: string }> {
+  ): Promise<{ id: string; email: string; access_token: string }> {
     const user = await this.usersService.findOne(email);
     if (!user && !(await bcrypt.compare(password, user.password))) {
       throw new UnauthorizedException();
     }
     const payload = { sub: user._id, email: user.email };
     return {
+      id: user._id,
+      email: user.email,
       access_token: await this.jwtService.signAsync(payload),
     };
   }
